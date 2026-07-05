97b30adf749a6579c207ed2d7ad6dc5fc9102aa7
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2026-07-05 11:40:42 +0100

    refactot(repo): Bumped version to 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,9 +12,8 @@ android {
         applicationId = "com.leadpresence.exportsignature"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.3"
-
+        versionCode = 4
+        versionName = "1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
